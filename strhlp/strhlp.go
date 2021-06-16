f3cfae06ae12ae480dcef28536dc696264770213
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2021-06-16 20:45:58 -0300

    (feat): search, map and filter funcs for string slices

--- strhlp/strhlp.go
+++ strhlp/strhlp.go
@@ -24,3 +24,37 @@ func Normalize(s string) string {
 	}
 	return r
 }
+
+// Search will search for a exact match of the string on the slide
+// provided and if found will return its index position, or -1 if not
+func Search(s string, list []string) int {
+	for i, sl := range list {
+		if sl == s {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// Map will apply the map function provided on every entry of
+// the string slice and return a slice with the changes
+func Map(f func(string) string, s []string) []string {
+	for i, e := range s {
+		s[i] = f(e)
+	}
+	return s
+}
+
+// Filter will run the function for every entry of the slice
+// and will return a new slice with the entries that return true
+// when used on the function
+func Filter(f func(string) bool, s []string) []string {
+	ns := make([]string, 0)
+	for _, e := range s {
+		if f(e) {
+			ns = append(ns, e)
+		}
+	}
+	return ns
+}
