ca41d1b5d3c81240e8ee0f8ecdad74b14d23df45
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-04-27 10:37:33 -0300

    feat: now/today report end alias

--- cmd/report.go
+++ cmd/report.go
@@ -33,7 +33,8 @@ var reportCmd = &cobra.Command{
 	Use:   "report [<start>] [<end>]",
 	Short: "List all time entries in the date ranges and with more data (format date as 2016-01-02)",
 	Long: "List all time entries in the date ranges and with more data (format date as 2016-01-02)\n" +
-		"If no parameter is set, shows today's time entries",
+		"If no parameter is set, shows today's time entries\n" +
+		"Aliases today/now can be used for <end> argument to represent current date",
 	Args:    cobra.MaximumNArgs(2),
 	PreRunE: printMultipleTimeEntriesPreRun,
 	Aliases: []string{"log"},
@@ -48,8 +49,9 @@ var reportCmd = &cobra.Command{
 
 		end := start
 		if len(args) > 1 {
-			end, err = time.Parse("2006-01-02", args[1])
-			if err != nil {
+			if args[1] == "now" || args[1] == "today" {
+				end = time.Now()
+			} else if end, err = time.Parse("2006-01-02", args[1]); err != nil {
 				return err
 			}
 		}
