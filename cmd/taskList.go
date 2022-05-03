2a4107b921cc09fdfd2ad19aed82b76feecd913f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-05-02 23:44:36 -0300

    feat: dedup task output

--- cmd/taskList.go
+++ cmd/taskList.go
@@ -16,12 +16,7 @@ limitations under the License.
 package cmd
 
 import (
-	"io"
-	"os"
-
 	"github.com/lucassabreu/clockify-cli/api"
-	"github.com/lucassabreu/clockify-cli/api/dto"
-	"github.com/lucassabreu/clockify-cli/internal/output"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -32,10 +27,6 @@ var taskListCmd = &cobra.Command{
 	Short:   "List tasks of a Clockify project",
 	Aliases: []string{"ls"},
 	RunE: withClockifyClient(func(cmd *cobra.Command, args []string, c *api.Client) error {
-		format, _ := cmd.Flags().GetString("format")
-		asJSON, _ := cmd.Flags().GetBool("json")
-		asCSV, _ := cmd.Flags().GetBool("csv")
-		quiet, _ := cmd.Flags().GetBool("quiet")
 		active, _ := cmd.Flags().GetBool("active")
 		name, _ := cmd.Flags().GetString("name")
 		project, _ := cmd.Flags().GetString("project")
@@ -63,26 +54,7 @@ var taskListCmd = &cobra.Command{
 			return err
 		}
 
-		var reportFn func([]dto.Task, io.Writer) error
-
-		reportFn = output.TaskPrint
-		if asJSON {
-			reportFn = output.TasksJSONPrint
-		}
-
-		if asCSV {
-			reportFn = output.TasksCSVPrint
-		}
-
-		if format != "" {
-			reportFn = output.TaskPrintWithTemplate(format)
-		}
-
-		if quiet {
-			reportFn = output.TaskPrintQuietly
-		}
-
-		return reportFn(tasks, os.Stdout)
+		return taskReport(cmd, tasks...)
 	}),
 }
 
@@ -90,11 +62,8 @@ func init() {
 	taskCmd.AddCommand(taskListCmd)
 
 	addProjectFlags(taskListCmd)
+	taskAddReportFlags(taskListCmd)
 
 	taskListCmd.Flags().StringP("name", "n", "", "will be used to filter the tag by name")
-	taskListCmd.Flags().StringP("format", "f", "", "golang text/template format to be applied on each Client")
 	taskListCmd.Flags().BoolP("active", "a", false, "display only active tasks")
-	taskListCmd.Flags().BoolP("json", "j", false, "print as JSON")
-	taskListCmd.Flags().BoolP("csv", "v", false, "print as CSV")
-	taskListCmd.Flags().BoolP("quiet", "q", false, "only display ids")
 }
