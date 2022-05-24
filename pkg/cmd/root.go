a984533de710a39ed6e32e0ba7aa6964ca844220
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-05-24 19:16:15 -0300

    feat: no need

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,17 +1,3 @@
-// Copyright © 2019 Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -21,7 +7,7 @@ import (
 	"github.com/lucassabreu/clockify-cli/pkg/cmd/project"
 	"github.com/lucassabreu/clockify-cli/pkg/cmd/tag"
 	"github.com/lucassabreu/clockify-cli/pkg/cmd/task"
-	"github.com/lucassabreu/clockify-cli/pkg/cmd/time-entry"
+	timeentry "github.com/lucassabreu/clockify-cli/pkg/cmd/time-entry"
 	"github.com/lucassabreu/clockify-cli/pkg/cmd/user"
 	"github.com/lucassabreu/clockify-cli/pkg/cmd/user/me"
 	"github.com/lucassabreu/clockify-cli/pkg/cmd/version"
