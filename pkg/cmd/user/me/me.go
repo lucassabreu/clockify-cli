d1cbe4411319ebfe3e88ac9ca64a72c7ed969029
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-05-31 08:44:12 -0300

    feat: user outputflags

--- pkg/cmd/user/me/me.go
+++ pkg/cmd/user/me/me.go
@@ -1,31 +1,21 @@
 package me
 
 import (
-	"io"
-
-	"github.com/lucassabreu/clockify-cli/api"
 	"github.com/lucassabreu/clockify-cli/api/dto"
+	"github.com/lucassabreu/clockify-cli/pkg/cmd/user/util"
 	"github.com/lucassabreu/clockify-cli/pkg/cmdutil"
-	output "github.com/lucassabreu/clockify-cli/pkg/output/user"
+	"github.com/lucassabreu/clockify-cli/pkg/output/user"
 	"github.com/spf13/cobra"
 )
 
 // NewCmdMe represents the me command
 func NewCmdMe(f cmdutil.Factory) *cobra.Command {
+	of := util.OutputFlags{}
 	cmd := &cobra.Command{
 		Use:   "me",
 		Short: "Show the user's token info",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			format, err := cmd.Flags().GetString("format")
-			if err != nil {
-				return err
-			}
-			asJSON, err := cmd.Flags().GetBool("json")
-			if err != nil {
-				return err
-			}
-			quiet, err := cmd.Flags().GetBool("quiet")
-			if err != nil {
+			if err := of.Check(); err != nil {
 				return err
 			}
 
@@ -34,42 +24,21 @@ func NewCmdMe(f cmdutil.Factory) *cobra.Command {
 				return err
 			}
 
-			u, err := run(c)
+			u, err := c.GetMe()
 			if err != nil {
 				return err
 			}
 
-			return report(u, cmd.OutOrStdout(), format, asJSON, quiet)
+			out := cmd.OutOrStdout()
+			if of.JSON {
+				return user.UserJSONPrint(u, out)
+			}
+
+			return util.Report([]dto.User{u}, out, of)
 		},
 	}
 
-	cmd.Flags().StringP("format", "f", "",
-		"golang text/template format to be applied on the user")
-	cmd.Flags().BoolP("json", "j", false, "print as json")
-	cmd.Flags().BoolP("quiet", "q", false, "only display ids")
+	util.AddReportFlags(cmd, &of)
 
 	return cmd
-}
-
-func run(c *api.Client) (dto.User, error) {
-	return c.GetMe()
-}
-
-func report(u dto.User, out io.Writer,
-	format string, asJSON, quiet bool) error {
-
-	if asJSON {
-		return output.UserJSONPrint(u, out)
-	}
-
-	list := []dto.User{u}
-	if format != "" {
-		return output.UserPrintWithTemplate(format)(list, out)
-	}
-
-	if quiet {
-		return output.UserPrintQuietly(list, out)
-	}
-
-	return output.UserPrint(list, out)
 }
--- pkg/cmd/user/user.go
+++ pkg/cmd/user/user.go
@@ -1,34 +1,23 @@
 package user
 
 import (
-	"io"
-
-	"github.com/lucassabreu/clockify-cli/api/dto"
-	"github.com/lucassabreu/clockify-cli/pkg/cmd/user/me"
-	"github.com/lucassabreu/clockify-cli/pkg/cmdutil"
-	output "github.com/lucassabreu/clockify-cli/pkg/output/user"
-
 	"github.com/lucassabreu/clockify-cli/api"
+	"github.com/lucassabreu/clockify-cli/pkg/cmd/user/me"
+	"github.com/lucassabreu/clockify-cli/pkg/cmd/user/util"
+	"github.com/lucassabreu/clockify-cli/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
 
 // NewCmdUser represents the users command
 func NewCmdUser(f cmdutil.Factory) *cobra.Command {
+	of := util.OutputFlags{}
 	cmd := &cobra.Command{
 		Use:     "user",
 		Aliases: []string{"user"},
 		Short:   "List all users on a Workspace",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			email, err := cmd.Flags().GetString("email")
-			if err != nil {
-				return err
-			}
-			format, err := cmd.Flags().GetString("format")
-			if err != nil {
-				return err
-			}
-			quiet, err := cmd.Flags().GetBool("quiet")
-			if err != nil {
+			email, _ := cmd.Flags().GetString("email")
+			if err := of.Check(); err != nil {
 				return err
 			}
 
@@ -42,43 +31,26 @@ func NewCmdUser(f cmdutil.Factory) *cobra.Command {
 				return err
 			}
 
-			users, err := run(c, w, email)
+			users, err := c.WorkspaceUsers(api.WorkspaceUsersParam{
+				Workspace: w,
+				Email:     email,
+			})
 			if err != nil {
 				return err
 			}
 
-			return report(users, cmd.OutOrStdout(), format, quiet)
+			return util.Report(users, cmd.OutOrStdout(), of)
 		},
 	}
 
 	cmd.Flags().StringP("email", "e", "",
 		"will be used to filter the workspaces by email")
-	cmd.Flags().StringP("format", "f", "",
-		"golang text/template format to be applied on each workspace")
-	cmd.Flags().BoolP("quiet", "q", false, "only display ids")
+
+	util.AddReportFlags(cmd, &of)
 
 	_ = cmd.MarkFlagRequired("workspace")
 
 	cmd.AddCommand(me.NewCmdMe(f))
 
 	return cmd
-
-}
-
-func run(c *api.Client, w, email string) ([]dto.User, error) {
-	return c.WorkspaceUsers(api.WorkspaceUsersParam{
-		Workspace: w,
-		Email:     email,
-	})
-}
-
-func report(u []dto.User, out io.Writer, format string, quiet bool) error {
-	switch {
-	case format != "":
-		return output.UserPrintWithTemplate(format)(u, out)
-	case quiet:
-		return output.UserPrintQuietly(u, out)
-	default:
-		return output.UserPrint(u, out)
-	}
 }
