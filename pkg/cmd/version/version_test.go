6a04ac69c2f3468a4b118c501c9f5cd2bbeb3a07
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-07-12 08:38:42 -0300

    feat: tests for pkg/cmd/version

---
+++ pkg/cmd/version/version_test.go
@@ -0,0 +1,58 @@
+package version_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/lucassabreu/clockify-cli/pkg/cmd/version"
+	"github.com/lucassabreu/clockify-cli/pkg/cmdutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersion(t *testing.T) {
+	type c struct {
+		name     string
+		version  cmdutil.Version
+		expected string
+	}
+	cases := []c{
+		c{
+			name:     "when no version",
+			version:  cmdutil.Version{},
+			expected: "Version: , Commit: , Build At:",
+		},
+		c{
+			name: "when default version",
+			version: cmdutil.Version{
+				Tag: "dev", Commit: "none", Date: "unknown"},
+			expected: "Version: dev, Commit: none, Build At: unknown",
+		},
+		c{
+			name: "with valid version",
+			version: cmdutil.Version{
+				Tag:    "1.0.0",
+				Commit: "63595df3d0dd6e4eef2e973631013ca9e06928ef",
+				Date:   "2022-07-01T04:10:47Z"},
+			expected: "Version: 1.0.0, " +
+				"Commit: 63595df3d0dd6e4eef2e973631013ca9e06928ef, " +
+				"Build At: 2022-07-01T04:10:47Z",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			b := bytes.NewBufferString("")
+			cmd := version.NewCmdVersion(&cmdutil.FactoryMock{
+				VersionFunc: func() cmdutil.Version { return tt.version },
+			})
+			cmd.SetArgs([]string{})
+			cmd.SetOut(b)
+			cmd.SetErr(b)
+			_, err := cmd.ExecuteC()
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, strings.TrimSpace(b.String()))
+		})
+	}
+}
