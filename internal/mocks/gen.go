37b92e1e8349be59ffea584beeb8c247bba23f79
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-11-06 11:33:24 -0300

    feat: EXPECT()

--- internal/mocks/gen.go
+++ internal/mocks/gen.go
@@ -5,17 +5,17 @@ import (
 	"github.com/lucassabreu/clockify-cli/pkg/cmdutil"
 )
 
-//go:generate mockery --name=Factory --inpackage
+//go:generate mockery --name=Factory --inpackage --with-expecter
 type Factory interface {
 	cmdutil.Factory
 }
 
-//go:generate mockery --name=Config --inpackage
+//go:generate mockery --name=Config --inpackage --with-expecter
 type Config interface {
 	cmdutil.Config
 }
 
-//go:generate mockery --name=Client --inpackage
+//go:generate mockery --name=Client --inpackage --with-expecter
 type Client interface {
 	api.Client
 }
