d97d0097345a86457552e4c4cbbaa41855bd872e
Arturo Volpe <arturovolpe@gmail.com>
2023-12-06 11:34:05 -0300

    feat: Support for times without leading '0' (#251)
    The web interface of clockify supports times in format HMM (1 digit for
    the hour and 2 digits for the minutes), for example 915 is 09:15.

--- pkg/timehlp/time.go
+++ pkg/timehlp/time.go
@@ -7,11 +7,14 @@ import (
 )
 
 const (
-	FullTimeFormat        = "2006-01-02 15:04:05"
-	SimplerTimeFormat     = "2006-01-02 15:04"
-	OnlyTimeFormat        = "15:04:05"
-	SimplerOnlyTimeFormat = "15:04"
-	NowTimeFormat         = "now"
+	FullTimeFormat           = "2006-01-02 15:04:05"
+	SimplerTimeFormat        = "2006-01-02 15:04"
+	OnlyTimeFormat           = "15:04:05"
+	SimplerOnlyTimeFormat    = "15:04"
+	SimplerOnlyTimeFormatWL  = "5:04"
+	NowTimeFormat            = "now"
+	SimplestOnlyTimeFormat   = "1504"
+	SimplestOnlyTimeFormatWL = "504"
 )
 
 // ConvertToTime will try to convert a string do time.Time looking for the
@@ -44,27 +47,61 @@ func ConvertToTime(timeString string) (t time.Time, err error) {
 	if len(FullTimeFormat) != l &&
 		len(SimplerTimeFormat) != l &&
 		len(OnlyTimeFormat) != l &&
-		len(SimplerOnlyTimeFormat) != l {
+		len(SimplerOnlyTimeFormat) != l &&
+		len(SimplestOnlyTimeFormat) != l &&
+		len(SimplestOnlyTimeFormatWL) != l {
 		return t, fmt.Errorf(
 			"supported formats are: %s",
 			strings.Join(
 				[]string{
 					FullTimeFormat, SimplerTimeFormat, OnlyTimeFormat,
-					SimplerOnlyTimeFormat, NowTimeFormat,
+					SimplerOnlyTimeFormat, SimplerOnlyTimeFormatWL, NowTimeFormat,
+					SimplestOnlyTimeFormat, SimplestOnlyTimeFormatWL,
 				},
 				", ",
 			),
 		)
 	}
 
+	timeString = normalizeFormats(timeString)
+
+	return time.ParseInLocation(FullTimeFormat, timeString, time.Local)
+}
+
+// Adds data to the partial timeString to match a full
+// datetime with seconds precission.
+// Receives a time in any of the defined formats, and return
+// a date in the FullTimeFormat
+func normalizeFormats(timeString string) string {
+	l := len(timeString)
+
+	// change from 9:14 to 09:14
+	if len(SimplerOnlyTimeFormatWL) == l && strings.Contains(timeString, ":") {
+		timeString = "0" + timeString
+		l = l + 1
+	}
+
+	// change from 914 to 0914
+	if len(SimplestOnlyTimeFormatWL) == l && !strings.Contains(timeString, ":") {
+		timeString = "0" + timeString
+		l = l + 1
+	}
+
+	// change from 0914 to 09:14
+	if len(SimplestOnlyTimeFormat) == l {
+		timeString = timeString[0:2] + ":" + timeString[2:]
+		l = l + 1
+	}
+
+	// change from 09:14 to 09:14:00
 	if len(SimplerOnlyTimeFormat) == l || len(SimplerTimeFormat) == l {
 		timeString = timeString + ":00"
 		l = l + 3
 	}
 
+	// change from 09:14 to 2006-01-02 09:14:00
 	if len(OnlyTimeFormat) == l {
 		timeString = Today().Format("2006-01-02") + " " + timeString
 	}
-
-	return time.ParseInLocation(FullTimeFormat, timeString, time.Local)
+	return timeString
 }
