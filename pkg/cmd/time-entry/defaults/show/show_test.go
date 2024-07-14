7c2428a93db49f7a2ad263890e9cd55c6cfdd440
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2024-07-14 11:41:14 -0300

    (wip)

--- pkg/cmd/time-entry/defaults/show/show_test.go
+++ pkg/cmd/time-entry/defaults/show/show_test.go
@@ -1,7 +1,64 @@
 package show_test
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/lucassabreu/clockify-cli/internal/mocks"
+	"github.com/lucassabreu/clockify-cli/pkg/cmd/time-entry/defaults/show"
+	"github.com/lucassabreu/clockify-cli/pkg/cmd/time-entry/util/defaults"
+	"github.com/stretchr/testify/assert"
+)
+
+var bFalse = false
 
 func TestNewCmdShow_ShouldPrintDefaults(t *testing.T) {
-	tts := []struct{}{}
+	dte := defaults.DefaultTimeEntry{
+		Workspace: "w",
+		ProjectID: "p",
+		Billable:  &bFalse,
+		TagIDs:    []string{"t1"},
+	}
+
+	ft := func(name string, args []string, expected string) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			f := mocks.NewMockFactory(t)
+
+			ted := mocks.NewMockTimeEntryDefaults(t)
+			f.EXPECT().TimeEntryDefaults().Return(ted)
+			ted.EXPECT().Read().Return(dte, nil)
+
+			cmd := show.NewCmdShow(f)
+			cmd.SilenceUsage = true
+			cmd.SilenceErrors = true
+
+			cmd.SetArgs(args)
+
+			out := bytes.NewBufferString("")
+
+			cmd.SetOut(out)
+			cmd.SetErr(out)
+
+			_, err := cmd.ExecuteC()
+
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			assert.Equal(t, expected, out.String())
+		})
+	}
+
+	ft("as json", []string{"--json"},
+		`{"workspace": "w","project":"p","billable":false,"tags":["t1"]}`)
+
+	ft("as yaml", []string{"--yaml"}, heredoc.Doc(`
+		workspace: w
+		project: p
+		billable: false
+		tags:
+		  - t1
+	`))
 }
