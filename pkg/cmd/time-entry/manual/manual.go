d117f727fe84e26825c7430275806f2901ff6225
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2024-07-23 08:59:26 -0300

    (wip)

--- pkg/cmd/time-entry/manual/manual.go
+++ pkg/cmd/time-entry/manual/manual.go
@@ -52,6 +52,11 @@ func NewCmdManual(f cmdutil.Factory) *cobra.Command {
 				return err
 			}
 
+			tei, err = util.FromDefaults(f)(tei)
+			if err != nil {
+				return err
+			}
+
 			c, err := f.Client()
 			if err != nil {
 				return err
