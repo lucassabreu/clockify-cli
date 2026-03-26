91123c07daafeb9428ec4548eb4362ae097fd76f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2026-03-26 09:32:46 -0300

    fix: more "ignored" errors

--- pkg/output/tag/quiet.go
+++ pkg/output/tag/quiet.go
@@ -10,7 +10,9 @@ import (
 // TagPrintQuietly will only print the IDs
 func TagPrintQuietly(ts []dto.Tag, w io.Writer) error {
 	for i := 0; i < len(ts); i++ {
-		fmt.Fprintln(w, ts[i].ID)
+		if _, err := fmt.Fprintln(w, ts[i].ID); err != nil {
+			return err
+		}
 	}
 
 	return nil
